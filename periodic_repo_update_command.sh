a71935434ddc14fc66a32ff014eb6949f78882ee
omerti <omertirosh@gmail.com>
2019-12-25 17:24:30 +0200

    git pull file

--- periodic_repo_update_command.sh
+++ periodic_repo_update_command.sh
@@ -2,6 +2,7 @@
 ### file to be used by crontab ###
 
 # in crontab configuration add the following line:
+# MAILTO=""
 # * * * * * /sternadi/home/volume1/shared/SternLab/periodic_repo_update_command.sh
 
 git -C /sternadi/home/volume1/shared/SternLab/ pull
