6d7dfe1350cccf25554f09d31a9b132c248efc9a
noamharel161 <36629785+noamharel161@users.noreply.github.com>
2020-01-05 10:11:48 +0200

    Update blast_utilities.py

--- blast_utilities.py
+++ blast_utilities.py
@@ -84,7 +84,7 @@ def get_unaligned_reads(blast_dir, fastq_file, out_file):
     file only with the unaligned reads.
     '''
     blasts = []
-    for b in [blast_dir + '/' + f for f in os.listdir(blast_dir) if f.endswith('.blast')]
+    for b in [blast_dir + '/' + f for f in os.listdir(blast_dir) if f.endswith('.blast')]:
         blasts.append(blast_to_df(b))
     blast = pd.concat(blasts)
     filter_out_fastq_by_read_id(fastq_file, out_file, blast.read_id.drop_duplicates().tolist())
