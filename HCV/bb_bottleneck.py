b625cc24b0fbe603f4ffded20844941d0319bbaa
Noam Harel <noamharel@powerlogin-be1.tau.ac.il>
2020-02-06 15:32:00 +0200

    hcv

--- HCV/bb_bottleneck.py
+++ HCV/bb_bottleneck.py
@@ -10,6 +10,8 @@ import os
 import subprocess
 import re
 from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 infection_order = {'HCV-P8':1, 'HCV-P1':2, 'HCV-P11':3, 'HCV-P7':5, 'HCV-P4':6, 'HCV-P3':7, 'HCV-P5':8, 'HCV-P2':9, 'HCV-P6':10, 'HCV-P9':11, 'HCV-P10':12, 'HCV-PS2':0}
@@ -24,45 +26,25 @@ def prepare_for_bb(donor_freq, recipient_freq, out_path):
     merged['Var_read_count_recipient'] = merged['Var_read_count_recipient'].round()
     merged = merged[merged.Pos.isin(range(738,2758))]
     # filtering - change this
+    # remove hvrs
     merged = merged[~(merged.Pos.isin(range(1479,1561))) & ~(merged.Pos.isin(range(1707,1783))) & ~(merged.Pos.isin(range(2037,2068)))]
     merged = merged[(merged.Ref != '-') & (merged.Var_read_count_recipient > 0) & (merged.Rank_donor == 0)]
     merged['Freq_donor'] = 1 - merged['Freq_donor']
     merged['Freq_recipient'] = 1 - merged['Freq_recipient']
-    #merged.loc[merged.Freq_recipient < 0.04, 'Freq_recipient'] = 0
-#    merged = merged[~merged.Pos.isin([1559.0,
-# 1181.0,
-# 1190.0,
-# 1938.0,
-# 2381.0,
-# 2390.0,
-# 2408.0,
-# 2594.0,
-# 2720.0,
-# 1493.0,
-# 1527.0,
-# 1630.0,
-# 1659.0,
-# 1666.0,
-# 1830.0,
-# 2582.0,
-# 1244.0,
-# 1580.0,
-# 1748.0,
-# 1946.0,
-# 2050.0,
-# 2715.0,
-# 905.0])]
+    merged.loc[merged.Freq_recipient < 0.04, 'Freq_recipient'] = 0
+    #merged.loc[merged.Freq_donor < 0.01, 'Freq_donor'] = 0
+    merged = merged[merged.Freq_donor >= 0.01]
     #
     merged[['Freq_donor', 'Freq_recipient', 'Read_count_donor', 'Var_read_count_recipient']].to_csv(out_path, header=False, index=False, sep='\t')
     return merged
 
-for f in ['Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/' + f for f in os.listdir('Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/')]:
+for f in ['Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/' + f for f in os.listdir('Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/') if f.count('-') < 2]:
     prepare_for_bb('Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/HCV-PS2.freqs', 
                f,
                'X:/volume2/noam/hcv/BB_bottleneck_output2/' + f.split('/')[-1].split('.')[0] + '.csv')
 
-for f in ['/sternadi/home/volume2/noam/hcv/BB_bottleneck_output/' + f for f in os.listdir('/sternadi/home/volume2/noam/hcv/BB_bottleneck_output/') if f.endswith('')]:
-    os.system('module load R/3.6.1 & Rscript /sternadi/home/volume2/noam/hcv/BB_bottleneck/Bottleneck_size_estimation_approx.r --file ' + f + ' > ' + f + '.bb.txt')
+#for f in ['/sternadi/home/volume2/noam/hcv/BB_bottleneck_output/' + f for f in os.listdir('/sternadi/home/volume2/noam/hcv/BB_bottleneck_output/') if f.endswith('')]:
+#    os.system('module load R/3.6.1 & Rscript /sternadi/home/volume2/noam/hcv/BB_bottleneck/Bottleneck_size_estimation_approx.r --file ' + f + ' > ' + f + '.bb.txt')
     
  
 
@@ -93,24 +75,12 @@ all_results = all_results[~(all_results.infection_order.isna()) & (all_results.S
 fig, ax = plt.subplots(nrows=1, ncols=1)
 slope, intercept, r_value, p_value, std_err = stats.linregress(all_results.infection_order, all_results.bottleneck)
 sns.regplot(all_results.infection_order, all_results.bottleneck, ax = ax)
-ax.set_title("y=%fx+%f, r2=%f" % (slope,intercept, r_value**2))
+ax.set_title("y=%fx+%f, r=%f, p=%f" % (slope,intercept, r_value, p_value))
 
 all_results = all_results[all_results.Sample != 'HCV-P4']
 fig, ax = plt.subplots(nrows=1, ncols=1)
 slope, intercept, r_value, p_value, std_err = stats.linregress(all_results.infection_order, all_results.bottleneck)
 sns.regplot(all_results.infection_order, all_results.bottleneck, ax = ax)
-ax.set_title("y=%fx+%f, r2=%f" % (slope,intercept, r_value**2))
+ax.set_title("y=%fx+%f, r=%f, p=%f" % (slope,intercept, r_value, p_value))
 
 
-
-
-
-#for f in ['Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/' + f for f in os.listdir('Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/')]:
-#    prepare_for_bb('Z:/volume1/noam/hcv_data/180423_TMS2-74068001_pipeline_optimized4/freqs/HCV-PS2.freqs', 
-#               f,
-#               'X:/volume2/noam/hcv/BB_bottleneck_output/' + f.split('/')[-1].split('.')[0] + '_all_variants.csv')
-
-for f in ['/sternadi/home/volume2/noam/hcv/BB_bottleneck_output/' + f for f in os.listdir('/sternadi/home/volume2/noam/hcv/BB_bottleneck_output/') if f.endswith('')]:
-    os.system('module load R/3.6.1 & Rscript /sternadi/home/volume2/noam/hcv/BB_bottleneck/Bottleneck_size_estimation_approx.r --file ' + f + ' > ' + f + '.bb.txt')
-    
-    
