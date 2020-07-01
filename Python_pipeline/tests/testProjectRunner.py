3d6923787ddaa70215d2fbb2412f7137676e9bf3
ita <itamar@trellis.ag>
2020-07-01 17:55:37 +0300

    testProjectRunner

---
+++ Python_pipeline/tests/testProjectRunner.py
@@ -0,0 +1,32 @@
+import argparse
+import unittest
+import filecmp
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Project_Runner import main
+
+
+class TestProjectRunner(unittest.TestCase):
+
+    def __init__(self):
+        self.output_dir = '/tmp/TestProjectRunner/'
+        parser = argparse.ArgumentParser()
+        parameters_dict = {
+            'input_dir': '/sternadi/home/volume3/ita/pipelineTester/small_data_samples/',
+            'output_dir': self.output_dir,
+            'ref': '/sternadi/home/volume3/ita/pipelineTester/test_data_reference.fasta'
+        }
+        for parameter, value in parameters_dict.items():
+            setattr(parser, parameter, value)
+        args = parser.parse_args()
+        main(args)
+
+    def assertAggregatedSummary(self):
+        AggregatedSummaryExample = '/sternadi/home/volume3/ita/pipelineTester/small_sample_results/AggregatedSummary.csv'
+        self.assertTrue(filecmp.cmp(f'{self.output_dir}/AggregatedSummary.csv', AggregatedSummaryExample),
+                        'AggregatedSummary does not match example..!')
+
+
+if __name__ == '__main__':
+    unittest.main()
