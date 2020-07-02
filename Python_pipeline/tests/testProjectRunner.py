a8402306d566638260a8b06ca2945f257a518ab0
ita <itamar@trellis.ag>
2020-07-02 13:25:55 +0300

    wip

--- Python_pipeline/tests/testProjectRunner.py
+++ Python_pipeline/tests/testProjectRunner.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Project_Runner import main, create_parser
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-from utils import pipeline_logger
+from utils.logger import pipeline_logger
 import subprocess
 
 
