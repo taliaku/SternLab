24a158756a97e52be45c482903b8c7f4244377ec
ita <itamar@trellis.ag>
2020-07-23 11:01:50 +0300

    wip

--- utils/logger.py
+++ utils/logger.py
@@ -31,6 +31,8 @@ def _create_new_logger(logger, log_file):
     # add ch & fh to logger
     logger.addHandler(ch)
     logger.addHandler(fh)
+    logger.info(f"pwd is: {os.system('pwd')}")
+    print(f"pwd is: {os.system('pwd')}")
     git_sha = subprocess.check_output("git rev-parse HEAD".split()).strip() #TODO: fix this.
     logger.info(f'Log started! Outputing to: {log_file}')
     logger.debug(f'git sha: {git_sha}')
