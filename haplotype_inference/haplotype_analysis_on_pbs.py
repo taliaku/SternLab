c329ee066c1800b505a28da3465a1374b391fb96
ita <itamar@trellis.ag>
2020-07-30 09:04:02 +0300

    add option to skip variants

--- haplotype_inference/haplotype_analysis_on_pbs.py
+++ haplotype_inference/haplotype_analysis_on_pbs.py
@@ -13,21 +13,26 @@ def main(args):
     os.makedirs(output_dir, exist_ok=True)
     input = args.input_dir
     num_of_jobs = args.number_of_bases
+    if args.skip_variants == "Y":
+        skip_variants = True
+    else:
+        skip_variants = False
     log = pipeline_logger(logger_name='Haplotype-Analysis', log_folder=output_dir)
     log.info(f"Starting haplotype analysis on folder {input} and outputting to {output_dir}")
     mutations_path = os.path.join(input, 'mutations_all.txt')
     blast_path = os.path.join(input, 'joined.blast')
     freqs_path = os.path.join(input, 'joined.freqs')
     this_dir_path = os.path.dirname(os.path.abspath(__file__))
-    variants_cmd = os.path.join(this_dir_path, 'variants_on_same_read.py')
     variants_folder = os.path.join(output_dir, 'variants')
-    os.makedirs(variants_folder, exist_ok=True)
-    cmd1 = f"python {variants_cmd} {blast_path} {mutations_path} $PBS_ARRAY_INDEX {freqs_path} > " \
-           f"{variants_folder}/$PBS_ARRAY_INDEX.txt"
-    cmd_path = os.path.join(output_dir, 'variants.cmd')
-    alias = 'Getting-Variants'
-    create_pbs_cmd(cmdfile=cmd_path, alias=alias, jnum=num_of_jobs, gmem=7, cmds=cmd1)
-    submit_wait_and_log(cmdfile=cmd_path, logger=log, job_name=alias)
+    if not skip_variants:
+        variants_cmd = os.path.join(this_dir_path, 'variants_on_same_read.py')
+        os.makedirs(variants_folder, exist_ok=True)
+        cmd1 = f"python {variants_cmd} {blast_path} {mutations_path} $PBS_ARRAY_INDEX {freqs_path} > " \
+               f"{variants_folder}/$PBS_ARRAY_INDEX.txt"
+        cmd_path = os.path.join(output_dir, 'variants.cmd')
+        alias = 'Haplotype-Analysis:Getting-Variants'
+        create_pbs_cmd(cmdfile=cmd_path, alias=alias, jnum=num_of_jobs, gmem=7, cmds=cmd1)
+        submit_wait_and_log(cmdfile=cmd_path, logger=log, job_name=alias) #TODO: get % done by number of files made
     linked_pairs_path = os.path.join(output_dir, "linked_pairs.txt")
     cmd2 = f"cat {variants_folder}/*.txt > {linked_pairs_path}"
     co_occurs_cmd = os.path.join(this_dir_path, 'co-occurs_to_stretches.py')
@@ -38,7 +43,7 @@ def main(args):
     cmds = cmd2 + "\n" + cmd3 + "\n" + cmd4
     cmd_path = os.path.join(output_dir, 'haplotype_analysis.cmd')
     alias = 'Haplotype-Analysis'
-    create_pbs_cmd(cmdfile=cmd_path, alias='Haplotype-Analysis - Graphing', cmds=cmds)
+    create_pbs_cmd(cmdfile=cmd_path, alias='Haplotype-Analysis:Graphing', cmds=cmds)
     submit_wait_and_log(cmdfile=cmd_path, logger=log, job_name=alias)
 
 
@@ -47,5 +52,6 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output_dir", help="Where all the files will go", required=True)
     parser.add_argument("-i", "--input_dir", help="Folder containing perl pipeline output", required=True)
     parser.add_argument("-n", "--number_of_bases", help="How many bases were sequenced?", default=3000) #TODO: do this automatically..
+    parser.add_argument("-s", "--skip_variants", help="Skip getting variants, default is N", default='N')
     args = parser.parse_args()
     main(args)
