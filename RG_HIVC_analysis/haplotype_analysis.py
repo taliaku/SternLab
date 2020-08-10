a1d57b7d893ffc3b262bd396fe96f890c3541abd
omerti <omertirosh@gmail.com>
2020-08-10 11:08:40 +0300

    HIVC

--- RG_HIVC_analysis/haplotype_analysis.py
+++ RG_HIVC_analysis/haplotype_analysis.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import itertools
 import sys
 import argparse
-
 from scipy.stats import chi2_contingency, fisher_exact
-
-
 import seaborn as sns;
+
+from RG_HIVC_analysis.constants import orig_first_samples, orig_high_quality_samples, RT_short_ET86_interval, \
+    control_excluded_patients, control_bl_samples
+from freqs_utilities import change_ref_to_consensus
 
 sns.set_context("poster")
 
@@ -69,31 +70,31 @@ def mutations_association(args):
             # 2. adjacent positions should be registered with joint freq, and given as input to aa_mut script?
 
 
-def freq_plot(freq_df):
-    samples = ['87415_S75', '504194_S38', '504202_S46', 'TASPX119494_S74']
-    freq_df = freq_df[freq_df['sample_id'].isin(samples)]
+def freq_plot(freq_df, fname, plot_header=''):
 
-    # advanced, chronological presentation
+    print('plotting freq_plot')
     g = sns.relplot(x= "Pos",
                     y= "Freq",
                     col='sample_id',
-                    # col_order='years_since_infection',
+                    # col_order='years_since_infection', # chronological presentation
                     # # hue='sample_id',
-                    # col_wrap=5,
+                    hue='mutation_type',
+                    col_wrap=7,
                     # join=True,
                     data=freq_df)
 
 
     # plot adjustments
     g.set(yscale="log")
-    plt.ylim(10**-4, 1)
+    plt.ylim(5e-4, 1)
+    g.fig.suptitle(plot_header, y= 0.1)
     # g.set_ylabels("mutation_rate")
     # g.set_xlabels("ET first sample (Years)")
     # g.set_xticklabels(rotation=45, fontsize=11)
 
     # extract plot
-    plt.show()
-    # plt.savefig(fname= '/Users/omer/PycharmProjects/SternLab/RG_HIVC_analysis/figures/mutation_rates_ET86_4s.png')
+    # plt.show()
+    plt.savefig(fname=fname)
     # g.savefig('')
 
 def dist_plot(freq_df):
@@ -128,22 +129,93 @@ def dist_plot(freq_df):
     # ax.set_xticks(range(0, 9000, 1000))
     plt.show()
 
+def dist_plot_v2(freq_df, fig_path, run_name, file_header=''):
+    print('plotting dist_plot_v2')
+    for sample in freq_df.sample_id.unique():
+    # for sample in ['504234_S78','504230_S74','504233_S77']:
+        freqs_per_sample = freq_df[(freq_df.sample_id == sample)]['Freq']
+        print(len(freqs_per_sample))
+        ax = sns.distplot(freqs_per_sample)
+        plot_header = file_header + '_' + str(sample)
+        ax.set_title(plot_header)
+        # ax.set(xscale="log")
+
+        # plt.show()
+        dist_fig_path = fig_path + '/mut_freq_distribution_figs/' + run_name
+        plt.savefig(dist_fig_path + '/' + plot_header + '_std_scale.png')
+        plt.cla()
 
 def main_plots():
-    # get freq file\s
-    # freq_df = pd.read_csv('/Users/omer/PycharmProjects/SternLab/RG_HIVC_analysis/ET86_4s/TASPX119494_S74.freqs', sep='\t')
-    freq_df = pd.read_csv('/Users/omer/PycharmProjects/SternLab/RG_HIVC_analysis/ET86_4s/unified_freqs_filtered_verbose.csv')
+    # get freq files
+    fig_path = '/Users/omer/PycharmProjects/SternLab/RG_HIVC_analysis/freq_trajectories/'
+    run_name = 'orig_high'
+    freq_df = pd.read_csv('/Users/omer/PycharmProjects/SternLab/RG_HIVC_analysis/runs/%s/unified_freqs_filtered_verbose.csv' % run_name)
+
+    # force ref=consensus in each sample (rank 0 == ref)
+    # TODO- redundant for new unified_freqs_filtered_verbose.csv
+    dfs = []
+    for sample in freq_df.sample_id.unique():
+        df_sample = freq_df[freq_df['sample_id'] == sample]
+        # TODO- remove indels before change_ref_to_consensus()
+        # df_sample_con_as_ref = change_ref_to_consensus(df_sample)
+        dfs.append(df_sample_con_as_ref)
+
+    freq_df = pd.concat(dfs)
+
 
     # filters
-    freq_df = freq_df[freq_df['Rank'] != 0]
+    freq_df = freq_df[freq_df['Rank'] == 1]
+    freq_df = freq_df[freq_df['Read_count'] > 100]
     freq_df = freq_df[freq_df['Freq'] != 0]
-    # freq_df = freq_df[freq_df['ind_id'].isin(['29447'])]
-    # freq_df = freq_df[freq_df['sample_id'].isin(['87415_S75', 'TASPX119494_S74'])]
-    # freq_df = freq_df[freq_df['sample_id'].isin(['87415_S75'])]
+    # freq_df = freq_df[freq_df['Freq'] > 0.01]
+    freq_df = freq_df[freq_df['Prob'] >= 0.95]
+    # freq_df = freq_df[(freq_df["Pos"] >= RT_short_ET86_interval[0]) & (freq_df["Pos"] <= RT_short_ET86_interval[1])]
 
-    # plot
-    freq_plot(freq_df)
-    # dist_plot(freq_df)
+    # transitions only
+    # freq_df["mutation_type"] = freq_df['Ref'] + freq_df['Base']
+    # freq_df = freq_df[freq_df["mutation_type"].isin(['GA', 'AG', 'GG', 'AA', 'CT', 'TC', 'CC', 'TT'])]
+    freq_df["mutation_type"] = freq_df['Ref'] + '->' + freq_df['Base']
+    mismathces = freq_df[freq_df["mutation_type"].isin(['G->G', 'A->A', 'C->C', 'T->T'])]
+    freq_df = freq_df[freq_df["mutation_type"].isin(['G->A', 'A->G', 'G->G', 'A->A', 'C->T', 'T->C', 'C->C', 'T->T'])]
+
+    # choose patients\samples
+    # patients = ['29447']
+    # patients = control_excluded_patients
+    # freq_df = freq_df[freq_df['ind_id'].isin(patients)]
+    # freq_df = freq_df[~freq_df['ind_id'].isin(patients)]
+
+    samples = orig_first_samples
+    # samples = control_bl_samples
+
+    # samples = orig_high_quality_samples
+    # samples = ['87415_S75', '504194_S38', '504202_S46', 'TASPX119494_S74'] # patient 15664
+    # samples = set(orig_first_samples).intersection(set(orig_high_quality_samples))
+
+    freq_df = freq_df[freq_df['sample_id'].isin(samples)]
+
+    file_header= '%s_rank1_cov_100_prob_95_transitions_founders_fixed_consensus' % run_name
+    # plot_header= '%s/Rank1, transitions only, founder samples, cov>100 per pos, prob >=0.95' % run_name
+    plot_header= file_header
+
+    fname = fig_path + '/muts_per_position_' + file_header
+    # report underlying statistics
+    mut_per_sample_stats = freq_df.groupby('sample_id').count()['Rank']
+    print('#mut per sample stats: \n' + str(mut_per_sample_stats.describe()))
+    mut_per_type_stats = freq_df.groupby('mutation_type').count()['Rank']
+    print('#mut per type stats: \n' + str(mut_per_type_stats))
+
+    # exporting to file
+    f = open(fname + '.txt', 'w')
+    f.write('#mut per sample stats: \n' + str(mut_per_sample_stats.describe()))
+    f.write('#mut per type stats: \n' + str(mut_per_type_stats))
+    f.close()
+
+    # plot1
+    plot_fname = fname + '.png'
+    freq_plot(freq_df, plot_fname, plot_header)
+
+    # plot2
+    # dist_plot_v2(freq_df, fig_path, run_name, file_header)
 
 
 def main_mutations_association():
