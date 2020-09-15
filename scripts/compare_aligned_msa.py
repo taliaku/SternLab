bebaf33f44a968bdc43b1c4df84403cdf7ff3449
Noam Harel <noamharel@power9login.tau.ac.il>
2020-09-15 09:50:33 +0300

    added grouping for indels

--- scripts/compare_aligned_msa.py
+++ scripts/compare_aligned_msa.py
@@ -4,6 +4,13 @@ from tqdm import tqdm
 import argparse
 import re
 
+
+def ranges(nums):
+    nums = sorted(set(nums))
+    gaps = [[s, e] for s, e in zip(nums, nums[1:]) if s + 1 < e]
+    edges = iter(nums[:1] + sum(gaps, []) + nums[-1:])
+    return list(zip(edges, edges))
+
 def compare_fastas_to_ref(fastas, ref_seq_name, output_csv, remove_edges=True):
     # fasta needs to be aligned, ref_seq_name is one of the sequences in the aligned fasta.
     with open(fastas) as f:
@@ -53,8 +60,36 @@ def compare_fastas_to_ref(fastas, ref_seq_name, output_csv, remove_edges=True):
 
     else:
         df.drop(columns=['fasta_position'], inplace=True)
-        
+
     df.sort_values(['sample', 'position']).to_csv(output_csv, index=False)
+
+    # group deletions
+    dels_df = df[(df.base == '-')].sort_values(['sample', 'position'])
+    dels_unified_df = []
+    for sample, group in dels_df.groupby('sample'):
+        sample_dels = group.sort_values('position').position.tolist()
+        sample_dels_unified = ranges(sample_dels)
+        for r in sample_dels_unified:
+            dels_unified_df.append((sample, r[0], r[1]))
+    dels_unified_df = pd.DataFrame(dels_unified_df, columns=['sample', 'start', 'end'])
+    dels_unified_df['length'] = dels_unified_df.end - dels_unified_df.start + 1
+    dels_unified_df.to_csv(output_csv.replace('.csv', '.deletions.csv'), index=False)
+
+    #group insertions
+    insertions_df = df[(df.ref_base == '-')].sort_values(['sample', 'position'])
+    insertions_unified_df = []
+    for sample, group in insertions_df.groupby('sample'):
+        sample_ins = group.sort_values('position').position.tolist()
+        sample_ins_unified = ranges(sample_ins)
+        for r in sample_ins_unified:
+            insertions_unified_df.append((sample, r[0], r[1]))
+    insertions_unified_df = pd.DataFrame(insertions_unified_df, columns=['sample', 'start', 'end'])
+    insertions_unified_df['start'] = insertions_unified_df.start.round()
+    insertions_unified_df['length'] = (insertions_unified_df.end - insertions_unified_df.start) * 1000
+    insertions_unified_df.drop(columns=['end'], inplace=True)
+    insertions_unified_df.to_csv(output_csv.replace('.csv', '.insertions.csv'), index=False)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
