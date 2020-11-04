256ed00b2fdb639810efe8c104fe50a6f4b40ec4
Noam Harel <noamharel161@gmail.com>
2020-11-04 10:20:04 +0200

    covid artic pipeline

---
+++ scripts/concat_dfs.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', help='files to concat', type=argparse.FileType('r'), nargs='+')
+    parser.add_argument('-o', '--out_file', help='out file', type=str)
+
+    args = parser.parse_args()
+    df = pd.concat([pd.read_csv(i) for i in args.files])
+    df.to_csv(args.out_file, index=False)
